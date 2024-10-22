0012de703a7acf8ae99ff6f6dc81502c16e641de
Szymon <sbozzek@gmail.com>
2024-10-22 18:21:51 +0200

    Nowy branch

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Test2");
+        System.out.println("Test2");
+        System.out.println("Test2");
+        System.out.println("Test2");
     }
 }
